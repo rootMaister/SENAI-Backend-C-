09c8ac6a34bcc591a92069ee54a1a0e9d106df61
FIC <FIC@info.local>
2019-09-13 11:15:00 -0300

    Primeiro Programa Funcionado

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -13,15 +13,53 @@ namespace aula1
             int num2 = 5;
 
             //Declaramos diversas variáveis de mesmo tipo
-            int num3, num4, num5 = 7;
+            int num3, num4, num5;
 
             //Variável do tipo decimal (real)
             float media;
             
             //Variável do tipo lógico (booleana)
             bool resultado = false;
+            
+            //Variavel do tipo texto(caracter)
+            string nomeAluno;
 
-            Console.WriteLine(num2 + num5);
+            Console.WriteLine("Bem vindos à nossa primeira aplicação!");
+            
+            //Quebra a Linha
+            Console.WriteLine("");
+
+            //Escreve em uma linha quebrand-a
+            Console.WriteLine("Digite seu nome");
+
+            //Continua escrevendo na mesma linha
+            Console.Write("Digite");
+            Console.Write(" seu nome:");
+
+            //Captura o dado digitado pelo usuário e salvo na variável
+            nomeAluno = Console.ReadLine();
+
+            //Concatenação
+            Console.WriteLine(nomeAluno + ", Agora digite sua primeira nota:" );
+
+            //Converto o text capturado para o tipo inteiro
+            num3 = int.Parse(Console.ReadLine());
+
+            //Interporlação
+            Console.WriteLine( $"Sua primeira nota {num3} ");
+            
+            num4 = int.Parse(Console.ReadLine());
+            Console.WriteLine( $"Digite sua segunda nota{num4} ");
+            
+            num5 = int.Parse(Console.ReadLine());
+            Console.WriteLine( $"Digite sua terceira nota{num5} ");
+            
+
+            media = (num3 + num4 + num5) / 3;
+
+            Console.WriteLine("Sua média é: " + media);
+
+
         }
     }
 }
