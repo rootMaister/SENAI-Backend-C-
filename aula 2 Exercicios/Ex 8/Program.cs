2d36123fec412242d5335869e6708bc029035dbc
FIC <FIC@info.local>
2019-09-17 13:48:46 -0300

    Exercicio 8 aula 2

---
+++ aula 2 Exercicios/Ex 8/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ex_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso, resultado, planeta;
+            
+            Console.WriteLine("Digite seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Em qual planeta você quer ser pesado ? ");
+            planeta = float.Parse(Console.ReadLine());
+
+            switch(planeta){
+                case 1:
+                resultado = (peso/10)*0.37f;
+                Console.WriteLine("Seu peso em Mercúrio seria " + resultado);
+                break;
+
+                case 2:
+                resultado = (peso/10)*0.88f/1;
+                Console.WriteLine("Seu peso em Vênus seria " + resultado);
+                break;
+
+                case 3:
+                resultado = (peso/10)*0.38f/1;
+                Console.WriteLine("Seu peso em Marte seria " + resultado);
+                break;
+
+                case 4:
+                resultado = (peso/10)*2.64f/1;
+                Console.WriteLine("Seu peso em Júpiter seria " + resultado);
+                break;
+
+                case 5:
+                resultado = (peso/10)*1.15f/1;
+                Console.WriteLine("Seu peso em Saturno seria " + resultado);
+                break;
+
+                case 6:
+                resultado = (peso/10)*1.17f/1;
+                Console.WriteLine("Seu peso em Urano seria " + resultado);
+                break;
+
+                default:
+                Console.WriteLine("Nunca vi peso escrito assim....");
+                break;
+             
+                
+            }
+
+            
+
+        }
+    }
+}
---
+++ aula 2 Exercicios/ex 7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ex_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            string pedido, nump;
+
+            Console.WriteLine("Digite seu pedido: ");
+            nump = Console.ReadLine();
+
+            switch(nump){
+                
+                case "1":
+                pedido = "Seu pedido é um Hamburguer.";
+                break;
+
+                case  "2":
+                pedido = "Seu pedido é um X - Salada.";
+                break;
+
+                case  "3":
+                pedido = "Seu pedido é um X - burguer.";
+                break;
+
+                case  "4":
+                pedido = "Seu pedido é um X - Bacon.";
+                break;
+
+                default:
+                pedido = "Número inválido, digite apenas NÚMEROS";
+                break;                
+            }
+
+            Console.WriteLine(pedido);
+        }
+    }
+}
